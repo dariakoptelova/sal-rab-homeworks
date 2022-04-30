694b597d22e97a68c2a512ef47c4f8a35cfe49e7
Daria Koptelova <da.koptelova@gmail.com>
2022-04-30 21:52:47 +0300

    ghgg

--- task_4/solution.js
+++ task_4/solution.js
@@ -18,10 +18,11 @@ function renderProductsCards(json){
 clearProducts(); 
 // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
 let product = parseProducts(json);
+}
 // Запишите в переменную length значение свойства products.length
 let length = products.length;
 // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
 for (let i = 0; i < length; i += 1); {
     addProduct(products[i]);
-}}
+}
