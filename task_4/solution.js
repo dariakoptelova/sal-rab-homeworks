0bb3478a45b6c1e8a5ee3d33ead3df3292dd545d
Daria Koptelova <da.koptelova@gmail.com>
2022-04-30 22:10:18 +0300

    12

--- task_4/solution.js
+++ task_4/solution.js
@@ -17,10 +17,9 @@ function renderProductsCards(json) {
 // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
 clearProducts();
 // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
-parseProducts(json);
-product = parseProducts(json);
+let product = parseProducts(json);
 // Запишите в переменную length значение свойства products.length
-let length = products.length;
+let length = product.length;
 // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
 for (let i = 0; i < length; i += 1); {
